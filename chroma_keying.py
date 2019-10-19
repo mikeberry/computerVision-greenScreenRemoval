444df336fe398b02633d625d19afe8fe40eaa034
michael <michael.an.meier@gmail.com>
2019-10-19 10:49:31 +0200

    only take values from the don't know zone of the trimap if they are not contained in the background neighborhood.

--- chroma_keying.py
+++ chroma_keying.py
@@ -2,27 +2,40 @@ import cv2
 import numpy as np
 import math
 
-def gaussian(x, mu, sigma):
-    return 1/(sigma * math.sqrt(2*math.pi))*math.exp(-1/2*math.pow((x-mu)/sigma,2))
 
 def logistic_cdf(x, mu, sigma):
-    return 1/(1+math.exp(-(x-mu)/sigma))
+    return 1 / (1 + math.exp(-(x - mu) / sigma))
+
 
 def dist_rgb(c1, c2):
     distB = math.pow(c1[0].astype(np.float), 2) - math.pow(c2[0].astype(np.float), 2)
     distG = math.pow(c1[1].astype(np.float), 2) - math.pow(c2[1].astype(np.float), 2)
     distR = math.pow(c1[2].astype(np.float), 2) - math.pow(c2[2].astype(np.float), 2)
-    dist = math.pow(math.pow(distB, 2) + math.pow(distG, 2) + math.pow(distR, 2),1/4)
+    dist = math.pow(math.pow(distB, 2) + math.pow(distG, 2) + math.pow(distR, 2), 1 / 4)
     return dist
 
+
 def minDistColor(img, mask, referenceColor):
-    #print(mask.shape)
-    #print(img.shape)
-    mask = mask.reshape((1,mask.shape[0],mask.shape[1]))
+    # print(mask.shape)
+    # print(img.shape)
+    mask = mask.reshape((1, mask.shape[0], mask.shape[1]))
     arr = img[tuple(mask == 1)]
-    diff = np.power(arr.astype(np.float),2) - np.power(referenceColor.astype(np.float),2)
-    minArg = np.argmin(np.power(np.power(diff[:,0], 2) + np.power(diff[:,1], 2) + np.power(diff[:,2], 2),1/4))
+    diff = np.power(arr.astype(np.float), 2) - np.power(referenceColor.astype(np.float), 2)
+    minArg = np.argmin(np.power(np.power(diff[:, 0], 2) + np.power(diff[:, 1], 2) + np.power(diff[:, 2], 2), 1 / 4))
     return arr[minArg]
+
+
+def hasSimilarBgr(A, B):
+    """
+    Returns true if one element of the list of BGR colors A is contained in a list of BGR colors B
+    :param A: list of BGR colors
+    :param B: list of BGR colors
+    :return: true if one element of the list of BGR colors A is contained in a list of BGR colors B, false otherwise
+    """
+    match = np.array([np.in1d(A[:, 0], B[:, 0]),
+                      np.in1d(A[:, 1], B[:, 1]),
+                      np.in1d(A[:, 2], B[:, 2])])
+    return np.any(np.logical_and(np.logical_and(match[0, :], match[1, :]), match[2, :]))
 
 
 def render_preview():
@@ -32,18 +45,18 @@ def render_preview():
     global selected_saturation
     global selected_value
     global tolerance
-    #hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_hue = max(selected_hue - tolerance / 100 * 30, 0)
     upper_hue = min(selected_hue + tolerance / 100 * 30, 255)
     lower_value = max(selected_value - 100, 0)
     upper_value = min(selected_value + 100, 255)
     lower_saturation = max(selected_value - 50, 0)
     upper_saturation = min(selected_value + 50, 255)
-    #lower_key = np.array([lower_hue, lower_saturation, lower_value])
-    #upper_key = np.array([upper_hue, upper_saturation, upper_value])
-    #mask = cv2.inRange(hsv_frame, lower_key, upper_key)
-    #preview = frame.copy()
-    #preview[mask != 0] = np.array([0, 0, 0])
+    # lower_key = np.array([lower_hue, lower_saturation, lower_value])
+    # upper_key = np.array([upper_hue, upper_saturation, upper_value])
+    # mask = cv2.inRange(hsv_frame, lower_key, upper_key)
+    # preview = frame.copy()
+    # preview[mask != 0] = np.array([0, 0, 0])
 
 
 def on_color_tolerance(tl):
@@ -53,7 +66,7 @@ def on_color_tolerance(tl):
 
 def on_softness(sl):
     global softness_level
-    if(sl==0):
+    if (sl == 0):
         softness_level = 1
     else:
         softness_level = sl
@@ -92,11 +105,11 @@ def generateTrimap(action, x, y, flags, userdata):
         backgroundHSV = hsv_frame[y, x, :]
         print(frame[y, x, :])
         iH, iS, iV = cv2.split(hsv_frame)
-        hb = np.where((iH<= backgroundHSV[0]+tolerance) & (iH>= backgroundHSV[0]- tolerance),1,0)
+        hb = np.where((iH <= backgroundHSV[0] + tolerance) & (iH >= backgroundHSV[0] - tolerance), 1, 0)
         print(hb.shape)
-        sb = np.where(iS >= 150,1,0)
+        sb = np.where(iS >= 150, 1, 0)
         print(sb.shape)
-        backgroundMask = np.logical_and(hb,sb).astype(np.uint8)
+        backgroundMask = np.logical_and(hb, sb).astype(np.uint8)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         backgroundMask = cv2.morphologyEx(backgroundMask, cv2.MORPH_CLOSE, kernel)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (21, 21))
@@ -111,7 +124,6 @@ def generateTrimap(action, x, y, flags, userdata):
         trimap = np.where(foregroundMask, 255, trimap)
         trimap = trimap.astype(np.uint8)
 
-
         alphaMask = np.zeros(trimap.shape)
 
         outputFrame = frame.copy()
@@ -120,41 +132,55 @@ def generateTrimap(action, x, y, flags, userdata):
                 if trimap[y, x] == 255:
                     alphaMask[y, x] = 255
                 elif trimap[y, x] == 127:
-                    starty = 0 if y - 60 < 0 else y -60
-                    endy = trimap.shape[0]-1 if y + 60 >= trimap.shape[0] else y + 60
-                    startx = 0 if x -60 <0 else x -60
-                    endx = trimap.shape[1]-1 if x + 60 >= trimap.shape[1] else x + 60
-                    localForegroundMask = foregroundMask[starty:endy,startx:endx]
-                    localForegroundMask = localForegroundMask.reshape((1,localForegroundMask.shape[0],localForegroundMask.shape[1]))
-                    meanForeground = np.mean(frame[starty:endy,startx:endx][tuple(localForegroundMask==1)].reshape(-1,3),axis=0).astype(np.uint8)
-                    localBackgroundMask = backgroundMask[starty:endy,startx:endx]
-                    localBackgroundMask = localBackgroundMask.reshape((1,localBackgroundMask.shape[0],localBackgroundMask.shape[1]))
-                    meanBackground = np.mean(frame[starty:endy,startx:endx][tuple(localBackgroundMask==1)].reshape(-1,3),axis=0).astype(np.uint8)
+                    starty = 0 if y - 60 < 0 else y - 60
+                    endy = trimap.shape[0] - 1 if y + 60 >= trimap.shape[0] else y + 60
+                    startx = 0 if x - 60 < 0 else x - 60
+                    endx = trimap.shape[1] - 1 if x + 60 >= trimap.shape[1] else x + 60
+                    localForegroundMask = foregroundMask[starty:endy, startx:endx]
+                    localForegroundMask = localForegroundMask.reshape(
+                        (1, localForegroundMask.shape[0], localForegroundMask.shape[1]))
+                    meanForeground = np.mean(
+                        frame[starty:endy, startx:endx][tuple(localForegroundMask == 1)].reshape(-1, 3), axis=0).astype(
+                        np.uint8)
+                    localBackgroundMask = backgroundMask[starty:endy, startx:endx]
+                    localBackgroundMask = localBackgroundMask.reshape(
+                        (1, localBackgroundMask.shape[0], localBackgroundMask.shape[1]))
+                    meanBackground = np.mean(
+                        frame[starty:endy, startx:endx][tuple(localBackgroundMask == 1)].reshape(-1, 3), axis=0).astype(
+                        np.uint8)
 
                     # Try minimum euclidean distance between the two arrays (fg and bg)
+                    foregroundColors = frame[starty:endy, startx:endx][tuple(localForegroundMask == 1)].reshape(-1, 3)
+                    backgroundColors = frame[starty:endy, startx:endx][tuple(localBackgroundMask == 1)].reshape(-1, 3)
+                    print(foregroundColors.shape)
+                    print(frame[y, x])
+                    print(backgroundColors)
+                    if hasSimilarBgr(frame[y, x].reshape(1, 3), backgroundColors):
+                        print("continue")
+                        alphaMask[y, x] = np.uint8(0)
+                        continue
+
                     distMean = dist_rgb(meanForeground, meanBackground)
 
                     if distMean == 0:
                         distMean = 0.1
 
-
-                    distX = dist_rgb(frame[y,x],meanBackground)/distMean
-
+                    distX = dist_rgb(frame[y, x], meanBackground) / distMean
 
                     # 0.16 is the solution of solve(1/(1+exp(-(0-0.5)/s))<0.05)
-                    sigma = 0.16 * softness_level/100
-                    alpha = logistic_cdf(distX, 0.5,sigma)
+                    sigma = 0.16 * softness_level / 100
+                    alpha = logistic_cdf(distX, 0.5, sigma)
                     if 0.99 > alpha > 0.05:
                         # foregroundNeighborhood = highlyLikelyForeground[starty:endy, startx:endx]
                         neighborhood = frame[starty:endy, startx:endx]
                         frame[y, x] = minDistColor(neighborhood, foregroundMask[starty:endy, startx:endx], frame[y, x])
 
-                    #if alpha > 0.95:
+                    # if alpha > 0.95:
                     #    alpha = 1.0
-                    #elif alpha < 0.1:
+                    # elif alpha < 0.1:
                     #    alpha = 0.0
-                    #alpha = alpha * 255
-                    #if alpha > 255:
+                    # alpha = alpha * 255
+                    # if alpha > 255:
                     #    alpha = 255
 
                     alpha = alpha * 255
@@ -168,7 +194,7 @@ def generateTrimap(action, x, y, flags, userdata):
 
                     alphaMask[y, x] = alpha
 
-        highlyLikelyAlphaMask = np.where(alphaMask==255,1,0)
+        highlyLikelyAlphaMask = np.where(alphaMask == 255, 1, 0)
         alphaMask = alphaMask.astype(np.uint8)
         alphaMask3d = cv2.merge((alphaMask, alphaMask, alphaMask)).astype(np.float)
         print(alphaMask)
@@ -181,12 +207,12 @@ def generateTrimap(action, x, y, flags, userdata):
         originalValue = np.array([0, 50, 100, 150, 200, 255])
 
         fullRange = np.arange(0, 256)
-        color_cast_percentage = color_cast_level/100
+        color_cast_percentage = color_cast_level / 100
         gCurve = np.array([0,
-                           50-color_cast_percentage*30,
-                           100-color_cast_percentage*50,
-                           150-color_cast_percentage*40,
-                           200-color_cast_percentage*20,
+                           50 - color_cast_percentage * 30,
+                           100 - color_cast_percentage * 50,
+                           150 - color_cast_percentage * 40,
+                           200 - color_cast_percentage * 20,
                            255])
         gLUT = np.interp(fullRange, originalValue, gCurve)
         gChannel = frame[:, :, 1]
@@ -196,7 +222,6 @@ def generateTrimap(action, x, y, flags, userdata):
         # Create a LookUp Table
         fullRange = np.arange(0, 256)
         gLUT = np.interp(fullRange, originalValue, gCurve)
-
 
         show_background = cv2.add(cv2.multiply(white.astype(np.float), (1 - alphaMask3d / 255)),
                                   cv2.multiply(frame.astype(np.float), alphaMask3d / 255))
