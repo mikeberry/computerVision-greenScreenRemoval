b4ae21ed0476a4779aade6628a940fd47885ce1e
michael <michael.an.meier@gmail.com>
2019-12-23 20:29:57 +0100

    add dist_rgb_test

---
+++ chroma_keying_test.py
@@ -0,0 +1,10 @@
+import pytest
+import chroma_keying
+import numpy as np
+
+
+def test_dist_rgb():
+    a = np.array([123, 230, 5])
+    b = np.array([13, 24, 255])
+    dist = chroma_keying.dist_rgb(a, b)
+    assert dist == pytest.approx(342.1, 0.1)
